f302f553e2ed433cfdf6b85521a5d032e1d25d95
Julio <julio.mech@hotmail.com>
2023-08-07 10:42:48 -0300

    First commit v0.1

---
+++ src/main/java/com/beerproject/projectbeer/ProjectbeerApplication.java
@@ -0,0 +1,13 @@
+package com.beerproject.projectbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beerproject/projectbeer/ProjectbeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerproject.projectbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
