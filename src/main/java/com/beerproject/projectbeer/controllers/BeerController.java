9cbe2e86a1898d919668692e4d437d4a512cc6c3
Julio <julio.mech@hotmail.com>
2023-08-09 14:54:45 -0300

    implement beer methods

--- src/main/java/com/beerproject/projectbeer/controllers/BeerController.java
+++ src/main/java/com/beerproject/projectbeer/controllers/BeerController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,19 @@ public class BeerController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Beer> findById(@PathVariable String id) {
+        Beer obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        service.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/beerproject/projectbeer/services/BeerService.java
+++ src/main/java/com/beerproject/projectbeer/services/BeerService.java
@@ -1,6 +1,7 @@
 package com.beerproject.projectbeer.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,17 @@ public class BeerService {
     public Beer insert(Beer obj) {
 
         return repository.insert(obj);
+    }
+
+    public Beer findById(String id) {
+
+        Optional<Beer> obj = repository.findById(id);
+        return obj.orElseThrow();
+    }
+
+    public void deleteById(String id) {
+        findById(id);
+
+        repository.deleteById(id);
     }
 }
